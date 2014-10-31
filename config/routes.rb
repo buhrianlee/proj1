e68c78e5608537bdeb84be50a6acffefcf8dfd16
buhrianlee <13leebrian@berkeley.edu>
2014-10-31 02:49:04 -0700

    I am the Pokemon God.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
+  patch '/capture', to: 'pokemon#capture', as: 'capture'
+  patch '/damage', to: 'pokemon#damage', as: 'damage'
+  post '/pokemon', to: 'pokemon#create'
+  get '/pokemon/new', to: 'pokemon#new', as: 'new_pokemon'
 end
